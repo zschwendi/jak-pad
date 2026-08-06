2e0438416fe9c5ee61ab5596fad61c9e537a833b
zephyrstudiosllc <zschwendi@zephyrstudios.dev>
2026-08-06 07:48:48 -0600

    build(jak1): record Build114 source pack identity

--- common/custom_data/Jak1SourceObjectPack.h
+++ common/custom_data/Jak1SourceObjectPack.h
@@ -14,7 +14,7 @@ namespace jak1_source_object_pack {
 
 inline constexpr const char* kManifestName = "object_pack_manifest.tsv";
 inline constexpr std::uint32_t kExpectedObjectCount = 518;
-inline constexpr std::uint64_t kRecordedAggregateXXH64 = 0xc2fa7910dd863ae7ULL;
+inline constexpr std::uint64_t kRecordedAggregateXXH64 = 0x2b812636de96f221ULL;
 
 enum class Phase {
   reading_manifest,
