8c0c48b1c1b8a2311d936f376da6b10083dc4f24
zephyrstudiosllc <zschwendi@zephyrstudios.dev>
2026-08-12 04:04:57 -0600

    Add host-managed adaptive 120 Hz timing

--- common/custom_data/Jak1SourceObjectPack.h
+++ common/custom_data/Jak1SourceObjectPack.h
@@ -14,7 +14,7 @@ namespace jak1_source_object_pack {
 
 inline constexpr const char* kManifestName = "object_pack_manifest.tsv";
 inline constexpr std::uint32_t kExpectedObjectCount = 518;
-inline constexpr std::uint64_t kRecordedAggregateXXH64 = 0x44f9fbb492ed188aULL;
+inline constexpr std::uint64_t kRecordedAggregateXXH64 = 0x8e8019d6c74ed9fcULL;
 
 enum class Phase {
   reading_manifest,
